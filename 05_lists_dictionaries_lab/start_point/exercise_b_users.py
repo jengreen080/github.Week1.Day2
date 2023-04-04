e222cb363a31a79a2d9c9827e58328c3deff70b8
Jennifer <jng-96@hotmail.co.uk>
2023-04-04 20:40:07 +0100

    trying to figure out how to use git

--- 05_lists_dictionaries_lab/start_point/exercise_b_users.py
+++ 05_lists_dictionaries_lab/start_point/exercise_b_users.py
@@ -55,6 +55,8 @@ users = {
 }
 
 # 1. Get Jonathan's Twitter handle (i.e. the string `"jonnyt"`)
+name = jen
+  print(name)
 # 2. Get Erik's hometown
 # 3. Get the list of Erik's lottery numbers
 # 4. Get the species of Avril's pet Monty
